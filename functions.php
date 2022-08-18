c7d7e3b1af417f9aa6eeae8b4ca1aba85c025d91
Tuomas Kämppi <tuomas.kamppi@metatavu.fi>
2022-08-18 18:09:32 +0300

    default colors and mobile styles

--- functions.php
+++ functions.php
@@ -1,3 +1,22 @@
 <?php
+/* Hook into the colours added via the customiser. */
+add_filter( 'theme_mod_accent_accessible_colors', 'op_change_default_colours', 10, 1 );
 
+/**
+ * Override the colours added in the customiser.
+ *
+ * @param array $default An array of the key colours being used in the theme.
+ */
+function op_change_default_colours( $default ) {
+
+    $default = array(
+        'content'       => array(
+            'accent'    => '##eb008c',
+            'secondary' => '#6d6d6d',
+            'borders'   => '#dcd7ca',
+        ),
+    );
+
+    return $default;
+}
 ?>
